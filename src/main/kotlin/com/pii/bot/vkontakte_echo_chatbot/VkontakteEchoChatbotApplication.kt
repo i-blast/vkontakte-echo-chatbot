2d384152b8d529c90006162e7e18674ebbdeef84
ilYa <EnableWebSocket@yandex.ru>
2025-03-26 20:22:57 +0500

    init

---
+++ src/main/kotlin/com/pii/bot/vkontakte_echo_chatbot/VkontakteEchoChatbotApplication.kt
@@ -0,0 +1,11 @@
+package com.pii.bot.vkontakte_echo_chatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkontakteEchoChatbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<VkontakteEchoChatbotApplication>(*args)
+}
---
+++ src/test/kotlin/com/pii/bot/vkontakte_echo_chatbot/VkontakteEchoChatbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pii.bot.vkontakte_echo_chatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkontakteEchoChatbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
